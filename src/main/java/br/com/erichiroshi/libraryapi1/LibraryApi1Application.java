3a5e102d5a73441f7f791fc929585af498357704
erichiroshi <erichiroshi@gmail.com>
2023-04-06 14:01:30 -0400

    first commit

---
+++ src/main/java/br/com/erichiroshi/libraryapi1/LibraryApi1Application.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.libraryapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erichiroshi/libraryapi1/LibraryApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erichiroshi.libraryapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
