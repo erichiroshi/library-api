2d0b25424dab8c75e8069beca1af796e48d4a126
erichiroshi <erichiroshi@hotmail.com>
2026-01-19 18:07:33 -0400

    feat: Add PostgreSQL and pgAdmin with Docker Compose and Flyway migrations

---
+++ src/main/java/com/example/library/security/ResourceSecurtyConfigDev.java
@@ -0,0 +1,32 @@
+package com.example.library.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Profile("dev")
+@Configuration
+public class ResourceSecurtyConfigDev {
+
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
+	    http.csrf(AbstractHttpConfigurer::disable)	     
+	    	.sessionManagement(session ->
+	            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	        )
+	        .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+	        )
+	        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+	    return http.build();
+	}
+
+}
